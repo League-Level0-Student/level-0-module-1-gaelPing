1200d22dc10f069bcb09c3cda0cfe4e7d72baea1
root <root@c808e06dec74>
2020-04-29 20:28:48 +0000

    have a nice day

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,25 @@
-
+PImage peppers;
+PImage olives;
+PImage pepperoni;
 void setup() {
-    
+ 
+ size(500,500);
+fill(#F0D400);
+ellipse(250,250,400,400);
+pepperoni = loadImage("download.jpeg");
+pepperoni.resize(50, 50);
+olives = loadImage("olives.jpeg");
+olives.resize(50, 50);
+peppers = loadImage("peppers.jpeg");
+peppers.resize(50,50);
 }
 void draw() {
-    
+if (mousePressed && (mouseButton == RIGHT)) {
+image(olives,mouseX,mouseY);
+}
+if(mousePressed && (mouseButton == LEFT)){
+image(pepperoni, mouseX, mouseY);
+}
+if(mousePressed && (mouseButton == CENTER)){
+image(peppers, mouseX, mouseY);}
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,11 +12,52 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+		String riddle=JOptionPane.showInputDialog(null, "What is greater than God,\r\n" + 
+				"more evil than the devil,\r\n" + 
+				"the poor have it,\r\n" + 
+				"the rich need it,\r\n" + 
+				"and if you eat it, you'll die?");
+		if( riddle.equals("nothing")) {
+			 JOptionPane.showMessageDialog(null, "CORRECT!");
+			 score++;
+		 }
+		
+		else {
+			JOptionPane.showMessageDialog(null, "WRRONG");
+			}
+		
+		 	riddle=JOptionPane.showInputDialog(null, "Who makes it, has no need of it.\r\n" + 
+		 			"Who buys it, has no use for it. \r\n" + 
+		 			"Who uses it can neither see nor feel it. \r\n" + 
+		 			"What is it?");
+		 	if( riddle.equals("coffin")) {
+		 		 JOptionPane.showMessageDialog(null, "CORRECT!");
+		 		 score++;
+		 	 }
+		 	
+		 	else {
+		 		JOptionPane.showMessageDialog(null, "WRRONG");
+		 		}
+		 	
+		 	riddle=JOptionPane.showInputDialog(null, "Brothers and sisters I have none but this man's father is my father's son.\r\n" + 
+		 			"Who is the man?");
+		 	
+		 	if( riddle.equals("your son")) {
+		 		 JOptionPane.showMessageDialog(null, "CORRECT!");
+		 		 score++;
+		 	 }
+		 	
+		 	else {
+		 		JOptionPane.showMessageDialog(null, "WRRONG");
+		 		}
+		 	
+		 	JOptionPane.showMessageDialog(null, "this is your final score : " + score);
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
 
+
+ }
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
@@ -21,5 +65,5 @@ public class TheRiddler {
 		// 2. Make a pop up to show the score.
 		
 	}
-}
+
 
